3c41f9c60d9c1a2ff0427accaa0d2a7edb11a288
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-13 18:30:58 -0300

    Aula 2

---
+++ aula2.js
@@ -0,0 +1,11 @@
+const {texto} =  require('./base');
+const regExp1 = /João|Maria/gi;
+    
+// (...() )(...)(...) $1 $2 $3
+
+
+
+// console.log(texto.match(regExp1));
+// console.log(texto.replace(/(João)/gi, (input) => {
+//     return input.toUpperCase();
+// }));
