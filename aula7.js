5f9813ed92dab46d626be0edea2385e8e1c52872
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-14 10:08:47 -0300

    Aula 7

---
+++ aula7.js
@@ -0,0 +1,13 @@
+const { cpfs2 } = require("./base");
+
+// ^ -> Começa com
+// $ -> termina com
+// [^] -> negação
+// m - multiline
+
+
+const cpf = '254.224.877-45';
+
+const cpfRegExp =/^(\d{3}\.){2}\d{3}\-\d{2}$/gm;
+
+console.log(cpfs2.match(cpfRegExp));
